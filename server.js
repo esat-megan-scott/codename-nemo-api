f5532b3ae03ee9d7e08d5f0cc3c274027f8c79d0
rscottlundgren <rscottlundgren@gmail.com>
2020-04-21 13:48:57 -0400

    Write "Create" Action for API - 1) Create File "app/models/answer.js" and wrote initial code for answer model. 2) Create File "app/models/survey.js" and wrote initial code for survey model. 3) Update User Model with reference to surveySchema and access to survey subdocuments. 4) Create File "app/routes/answer_routes.js" and wrote initial code for answer route. 5) Create File "app/models/survey_routes.js" and wrote initial code for survey route. 6) Duplicated Example curl-scripts into a new "Surveys" folder. 7) Updated "Create" curl-scripts with new key value pairs. 8) Required and registered route files.
    Close #2

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
+const answerRoutes = require('./app/routes/answer_routes')
+const surveyRoutes = require('./app/routes/survey_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -63,6 +65,8 @@ app.use(requestLogger)
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
+app.use(answerRoutes)
+app.use(surveyRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
